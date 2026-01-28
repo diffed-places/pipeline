f2b8eca1dfee325f30711986edb5fbb3dccbcdcc
Sascha Brawer <sascha@brawer.ch>
2026-01-28 17:30:47 +0100

    Prune module exports (#94)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
-pub mod coverage;
-pub mod import_atp;
-pub mod import_osm;
-pub mod place;
+mod coverage;
+mod import_atp;
+mod import_osm;
+mod place;
+
+// Re-exported for main.rs.
+pub use coverage::build_coverage;
+pub use import_atp::import_atp;
+pub use import_osm::import_osm;
+
+#[cfg(fuzzing)]
+pub use import_atp::fuzz::fuzz_process_geojson;
