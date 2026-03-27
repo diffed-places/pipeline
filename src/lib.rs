7aae5ea6979737065d894db14f087c77b51f1a79
Sascha Brawer <sascha@brawer.ch>
2026-03-27 11:43:58 +0100

    Compute bitmasks for feature matching, increase matching distance (#217)
    We now use a maximal matching distance of 400 meters for shops, 800 meters
    for railway platforms, and 10 meters for “tiny” features such as hydrants,
    trees or trash cans.
    
    We now partition both ATP and OSM features into “matching classes”
    such as `SHOP` or `STREET_FURNITURE`. A single feature may belong
    to multiple classes. This is not intended as yet another POI category
    system; we will only use it to restrict the input to actual feature
    matching.
    
    The pipeline now filters out any features that don’t belong to any
    matching class, thus excluding streets or buildings that aren’t of
    interest to the diffed places project.
    
    Tmings on a 2018 MacBook Pro with 6 Intel CPUs after this change:
    
    | Phase            | Walltime |
    |------------------|---------:|
    | atp.fetch        |     76 s |
    | atp.read         |     51 s |
    | atp.write        |    186 s |
    | coverage.read    |   1101 s |
    | coverage.write   |   1337 s |
    | osm.fetch        |        — |
    | osm.prt.r        |      1 s |
    | osm.cov.n        |    741 s |
    | osm.cov.w        |    261 s |
    | osm.cov.r        |      6 s |
    | osm.filter.r     |     57 s |
    | osm.filter.w     |   3308 s |
    | osm.filter.n     |   5846 s |
    | osm.assemble.r   |   1403 s |
    | osm.assemble.w   |    842 s |
    
    The generated Parquet file is now 293 MiB for AllThePlaces,
    and 938 MiB for OpenStreetMap.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use deepsize::DeepSizeOf;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use serde::{Deserialize, Serialize};
+
 mod atp;
 mod coverage;
 mod osm;
@@ -8,8 +12,6 @@ mod u64_table;
 pub use atp::import_atp;
 pub use coverage::build_coverage;
 pub use osm::import_osm;
-
-use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 
 #[cfg(fuzzing)]
 pub use import_atp::fuzz::fuzz_process_geojson;
@@ -53,5 +55,170 @@ fn make_download_bar(
         let style = ProgressStyle::with_template(SPINNER_STYLE).expect("bad SPINNER_STYLE");
         bar.set_style(style);
         bar
+    }
+}
+
+#[derive(Debug, DeepSizeOf, Default, Deserialize, PartialEq, Eq, Ord, PartialOrd, Serialize)]
+#[repr(transparent)]
+struct MatchMask(u16);
+
+impl MatchMask {
+    pub const SHOP: MatchMask = MatchMask(1 << 0);
+    pub const RESTAURANT: MatchMask = MatchMask(1 << 1);
+    pub const LODGING: MatchMask = MatchMask(1 << 2);
+    pub const SCHOOL: MatchMask = MatchMask(1 << 3);
+    pub const TRANSIT: MatchMask = MatchMask(1 << 4);
+    pub const PARKING: MatchMask = MatchMask(1 << 5);
+    pub const FUEL: MatchMask = MatchMask(1 << 6);
+    pub const SHRUBBERY: MatchMask = MatchMask(1 << 7);
+    pub const STREET_FURNITURE: MatchMask = MatchMask(1 << 8);
+
+    pub fn is_empty(&self) -> bool {
+        self.0 == 0
+    }
+
+    pub fn add_tag(&mut self, key: &str, value: &str) {
+        self.0 |= match (key, value) {
+            ("amenity", "arts_centre") => Self::SHOP.0,
+            ("amenity", "bank") => Self::SHOP.0,
+            ("amenity", "bench") => Self::STREET_FURNITURE.0,
+            ("amenity", "bicycle_rental") => Self::SHOP.0,
+            ("amenity", "bbq") => Self::STREET_FURNITURE.0,
+            ("amenity", "bicycle_repair_station") => Self::STREET_FURNITURE.0,
+            ("amenity", "bureau_de_change") => Self::SHOP.0,
+            ("amenity", "childcare") => Self::SCHOOL.0,
+            ("amenity", "dentist") => Self::SHOP.0,
+            ("amenity", "drinking_water") => Self::STREET_FURNITURE.0,
+            ("amenity", "driving_school") => Self::SCHOOL.0,
+            ("amenity", "charging_station") => Self::FUEL.0,
+            ("amenity", "ferry_terminal") => Self::TRANSIT.0,
+            ("amenity", "fountain") => Self::STREET_FURNITURE.0,
+            ("amenity", "fuel") => Self::FUEL.0,
+            ("amenity", "grit_bin") => Self::STREET_FURNITURE.0,
+            ("amenity", "ice_cream") => Self::SHOP.0,
+            ("amenity", "kindergarten") => Self::SCHOOL.0,
+            ("amenity", "letter_box") => Self::STREET_FURNITURE.0,
+            ("amenity", "library") => Self::SHOP.0,
+            ("amenity", "motorcycle_parking") => Self::PARKING.0,
+            ("amenity", "nightclub") => Self::SHOP.0,
+            ("amenity", "parking") => Self::PARKING.0,
+            ("amenity", "pharmacy") => Self::SHOP.0,
+            ("amenity", "place_of_worship") => Self::SHOP.0,
+            ("amenity", "post_box") => Self::STREET_FURNITURE.0,
+            ("amenity", "post_office") => Self::SHOP.0,
+            ("amenity", "public_bookcase") => Self::STREET_FURNITURE.0,
+            ("amenity", "pub") => Self::RESTAURANT.0,
+            ("amenity", "recycling") => Self::STREET_FURNITURE.0,
+            ("amenity", "studio") => Self::SHOP.0,
+            ("amenity", "taxi") => Self::TRANSIT.0,
+            ("amenity", "telephone") => Self::STREET_FURNITURE.0,
+            ("amenity", "theatre") => Self::SHOP.0,
+            ("amenity", "trolley_bay") => Self::STREET_FURNITURE.0,
+            ("amenity", "restaurant") => Self::RESTAURANT.0,
+            ("amenity", "university") => Self::SCHOOL.0,
+            ("amenity", "vending_machine") => Self::STREET_FURNITURE.0,
+            ("amenity", "waste_basket") => Self::STREET_FURNITURE.0,
+            ("amenity", "water_point") => Self::STREET_FURNITURE.0,
+            ("amenity", "watering_place") => Self::STREET_FURNITURE.0,
+
+            ("barrier", "fence") => Self::STREET_FURNITURE.0,
+            ("barrier", "gate") => Self::STREET_FURNITURE.0,
+            ("barrier", "hedge") => Self::SHRUBBERY.0,
+            ("barrier", "stile") => Self::STREET_FURNITURE.0,
+
+            ("emergency", "acess_point") => Self::STREET_FURNITURE.0,
+            ("emergency", "assembly_point") => Self::STREET_FURNITURE.0,
+            ("emergency", "bleed_control_kit") => Self::STREET_FURNITURE.0,
+            ("emergency", "defibrillator") => Self::STREET_FURNITURE.0,
+            ("emergency", "disaster_help_point") => Self::STREET_FURNITURE.0,
+            ("emergency", "fire_alarm_box") => Self::STREET_FURNITURE.0,
+            ("emergency", "fire_hydrant") => Self::STREET_FURNITURE.0,
+            ("emergency", "first_aid_kit") => Self::STREET_FURNITURE.0,
+            ("emergency", "key_depot") => Self::STREET_FURNITURE.0,
+            ("emergency", "rescue_box") => Self::STREET_FURNITURE.0,
+            ("emergency", "shower") => Self::STREET_FURNITURE.0,
+
+            ("highway", "bus_stop") => Self::TRANSIT.0,
+            ("highway", "elevator") => Self::STREET_FURNITURE.0,
+            ("highway", "fake_speed_camera") => Self::STREET_FURNITURE.0,
+            ("highway", "ladder") => Self::STREET_FURNITURE.0,
+            ("highway", "milestone") => Self::STREET_FURNITURE.0,
+            ("highway", "speed_camera") => Self::STREET_FURNITURE.0,
+            ("highway", "street_lamp") => Self::STREET_FURNITURE.0,
+            ("highway", "traffic_sign") => Self::STREET_FURNITURE.0,
+            ("highway", "traffic_signals") => Self::STREET_FURNITURE.0,
+
+            ("historic", "boundary_stone") => Self::STREET_FURNITURE.0,
+            ("historic", "wayside_cross") => Self::STREET_FURNITURE.0,
+            ("historic", "wayside_shrine") => Self::STREET_FURNITURE.0,
+
+            ("leisure", "adult_gaming_centre") => Self::SHOP.0,
+            ("leisure", "amusement_arcade") => Self::SHOP.0,
+            ("leisure", "bandstand") => Self::STREET_FURNITURE.0,
+            ("leisure", "bird_hide") => Self::STREET_FURNITURE.0,
+            ("leisure", "bowling_alley") => Self::SHOP.0,
+            ("leisure", "dance") => Self::SHOP.0,
+            ("leisure", "minigolf") => Self::SHOP.0,
+            ("leisure", "firepit") => Self::STREET_FURNITURE.0,
+            ("leisure", "hot_tub") => Self::STREET_FURNITURE.0,
+            ("leisure", "playground") => Self::SHOP.0,
+            ("leisure", "picnic_table") => Self::STREET_FURNITURE.0,
+            ("leisure", "resort") => Self::SHOP.0,
+            ("leisure", "water_park") => Self::SHOP.0,
+
+            ("man_made", "cutline") => 0,
+            ("man_made", _) => Self::STREET_FURNITURE.0,
+
+            ("natural", "bush") => Self::SHRUBBERY.0,
+            ("natural", "heath") => Self::SHRUBBERY.0,
+            ("natural", "plant") => Self::SHRUBBERY.0,
+            ("natural", "shrub") => Self::SHRUBBERY.0,
+            ("natural", "shrubbery") => Self::SHRUBBERY.0,
+            ("natural", "tree") => Self::SHRUBBERY.0,
+            ("natural", "trees") => Self::SHRUBBERY.0,
+            ("natural", "tree_group") => Self::SHRUBBERY.0,
+            ("natural", "tree_row") => Self::SHRUBBERY.0,
+
+            ("power", "cable") => 0,
+            ("power", "line") => 0,
+            ("power", "minor_cable") => 0,
+            ("power", "minor_line") => 0,
+            ("power", "no") => 0,
+            ("power", "yes") => 0,
+            ("power", _) => Self::STREET_FURNITURE.0,
+
+            ("public_transport", "no") => 0,
+            ("public_transport", _) => Self::TRANSIT.0,
+
+            ("railway", "platform") => Self::TRANSIT.0,
+
+            ("school", "no") => 0,
+            ("school", _) => Self::SCHOOL.0,
+
+            ("shop", "no") => 0,
+            ("shop", _) => Self::SHOP.0,
+
+            ("tourism", "alpine_hut") => Self::LODGING.0,
+            ("tourism", "apartment") => Self::LODGING.0,
+            ("tourism", "apartments") => Self::LODGING.0,
+            ("tourism", "cabin") => Self::LODGING.0,
+            ("tourism", "camp_site") => Self::LODGING.0,
+            ("tourism", "caravan_site") => Self::LODGING.0,
+            ("tourism", "guest_house") => Self::LODGING.0,
+            ("tourism", "hostel") => Self::LODGING.0,
+            ("tourism", "hotel") => Self::LODGING.0,
+            ("tourism", "hotel;motel") => Self::LODGING.0,
+            ("tourism", "motel") => Self::LODGING.0,
+            ("tourism", "wilderness_hut") => Self::LODGING.0,
+
+            ("tower", _) => Self::STREET_FURNITURE.0,
+
+            _ => 0,
+        }
+    }
+
+    #[allow(unused)] // TODO: Remove attribute once we use it.
+    pub fn intersects(&self, other: &Self) -> bool {
+        (self.0 | other.0) != 0
     }
 }
