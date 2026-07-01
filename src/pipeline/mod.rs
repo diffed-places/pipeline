16f3ac455eca42611dee01c6901ed00dbde27b2f
Sascha Brawer <sascha@brawer.ch>
2026-07-01 11:08:29 +0200

    Add helper to find the last-modified time for a slice of paths

--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -1,5 +1,5 @@
-use anyhow::{Ok, Result};
-use std::path::Path;
+use anyhow::{Context, Ok, Result};
+use std::{path::Path, time::SystemTime};
 
 mod edits;
 mod tiles;
@@ -22,4 +22,60 @@ pub fn run_pipeline(http_client: &reqwest::Client, workdir: &Path) -> Result<()>
     upload::upload_tiles(&tiles, &progress)?;
 
     Ok(())
+}
+
+/// Returns the highest (most recent) last modification time among all paths.
+/// If any path does not exist or cannot be accessed, an error is returned.
+#[allow(unused)] // TODO: Remove once this function is used in production code.
+fn last_modified(paths: &[&Path]) -> Result<SystemTime> {
+    if !paths.is_empty() {
+        let mut last = modified(paths[0])?;
+        for path in &paths[1..] {
+            last = last.max(modified(path)?);
+        }
+        Ok(last)
+    } else {
+        anyhow::bail!("paths should not be empty")
+    }
+}
+
+fn modified(path: &Path) -> Result<SystemTime> {
+    std::fs::metadata(path)
+        .with_context(|| format!("Failed to get metadata for path: {:?}", path))?
+        .modified()
+        .with_context(|| format!("Failed to get modification time for path: {:?}", path))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{ops::Add, time::Duration};
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn test_last_modified() -> Result<()> {
+        let f0 = NamedTempFile::new()?;
+        let f2 = NamedTempFile::new()?;
+        let f7 = NamedTempFile::new()?;
+
+        let t0 = SystemTime::now();
+        let t2 = t0.add(Duration::new(2, 0));
+        let t7 = t0.add(Duration::new(7, 0));
+
+        f0.as_file().set_modified(t0)?;
+        f2.as_file().set_modified(t2)?;
+        f7.as_file().set_modified(t7)?;
+
+        assert!(last_modified(&[]).is_err());
+        assert!(last_modified(&[&Path::new("/no/such/file")]).is_err());
+
+        assert_eq!(last_modified(&[f0.path()])?, t0);
+        assert_eq!(last_modified(&[f0.path(), f2.path()])?, t2);
+        assert_eq!(last_modified(&[f2.path(), f0.path()])?, t2);
+        assert_eq!(last_modified(&[f0.path(), f2.path(), f7.path()])?, t7);
+        assert_eq!(last_modified(&[f0.path(), f7.path(), f2.path()])?, t7);
+        assert_eq!(last_modified(&[f7.path(), f2.path(), f0.path()])?, t7);
+
+        Ok(())
+    }
 }
